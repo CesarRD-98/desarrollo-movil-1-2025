2c1bf75f6bd82950cb37a84a7a6d8f351ca29aac
CesarRD-98 <dedioscesar12@gmail.com>
2025-05-13 20:43:57 -0600

    update

--- Semana 4/hooks-context-api/provider/estudianteProvider.tsx
+++ Semana 4/hooks-context-api/provider/estudianteProvider.tsx
@@ -5,30 +5,28 @@ import { EstudianteContext } from '../context/estudianteContext';
 export default function EstudianteProvider({ children }: { children: ReactNode }) {
 
     const [estudiantes, setEstudiantes] = useState<Estudiante[]>([
-        { id: 1, nombre: 'Juan' },
-        { id: 2, nombre: 'Maria' },
-        { id: 3, nombre: 'Pedro' },
-        { id: 4, nombre: 'Ana' },
-        { id: 5, nombre: 'Luis' },
-        { id: 6, nombre: 'Laura' },
-        { id: 7, nombre: 'Carlos' },
-        { id: 8, nombre: 'Sofia' },
-        { id: 9, nombre: 'Javier' },
-        { id: 10, nombre: 'Isabel' }
+        { id: 1, nombre: 'Estudiante Juan' },
+        { id: 2, nombre: 'Estudiante Maria' },
+        { id: 3, nombre: 'Estudiante Pedro' },
+        { id: 4, nombre: 'Estudiante Ana' },
+        { id: 5, nombre: 'Estudiante Luis' },
+        { id: 6, nombre: 'Estudiante Laura' },
+        { id: 7, nombre: 'Estudiante Carlos' },
+        { id: 8, nombre: 'Estudiante Sofia' },
+        { id: 9, nombre: 'Estudiante Javier' },
+        { id: 10, nombre: 'Estudiante Isabel' }
     ])
 
     useEffect(() => {
-        const timer = setInterval(() => {
+        const tiempo = setInterval(() => {
             setEstudiantes((prevEstudiante) => {
                 const nuevoEstudiante: Estudiante = { id: prevEstudiante.length + 1, nombre: `Estudiante ${prevEstudiante.length + 1}` }
                 return [...prevEstudiante, nuevoEstudiante]
             });
         }, 5000);
 
-        return () => clearInterval(timer);
+        return () => clearInterval(tiempo);
     }, []);
-
-
 
     return (
         <EstudianteContext.Provider value={{ estudiantes }}>
