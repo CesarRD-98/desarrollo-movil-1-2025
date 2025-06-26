e4aa181b970f97eabe606b143cea384f20879f98
CesarRD-98 <dedioscesar12@gmail.com>
2025-06-26 16:02:01 -0600

    fix

--- Semana 10/Examen-2-parcial/backend-examen/routes/producto.route.js
+++ Semana 10/Examen-2-parcial/backend-examen/routes/producto.route.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const productoController = require('../controllers/producto.controller')
 
 router.get('/productos', productoController.getProductos)
-router.post('productos', productoController.createProducto)
-router.delete('items/:id', productoController.deleteProducto)
+router.post('/productos', productoController.createProducto)
+router.delete('/items/:id', productoController.deleteProducto)
 
 module.exports = router
